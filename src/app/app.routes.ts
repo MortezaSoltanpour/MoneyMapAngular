8aa3181dc1ba3a185e1a8d87c6e6f6dab6a2f172
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-17 14:56:15 -0300

    Header,Footer, Login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AdminContainerComponent } from './pages/layout/admin-container/admin-container.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { component: LoginComponent, path: '' },
+  {
+    component: AdminContainerComponent,
+    path: 'financial',
+    children: [
+      {
+        component: DashboardComponent,
+        path: 'dashboard',
+      },
+    ],
+  },
+];
