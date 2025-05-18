8d245b4a7ef8b28a7660a7c2d2c7ff8406ff6e04
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-17 22:59:04 -0300

    Refactor dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AdminContainerComponent } from './pages/layout/admin-container/admin-container.component';
 import { LoginComponent } from './pages/login/login.component';
 import { TransactionsComponent } from './pages/transactions/transactions.component';
 import { UsersComponent } from './pages/users/users.component';
 import { CategoryListComponent } from './domain/categories/pages/category-list/category-list.component';
+import { DashboardComponent } from './domain/dashboard/pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { component: LoginComponent, path: '' },
