60140f0c805d742b7d82c4e8a1f60d3a913d0924
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-19 18:28:58 -0300

    Define api end points

--- src/app/shared/apiAddress.ts
+++ src/app/shared/apiAddress.ts
@@ -1,4 +1,9 @@
 export class ApiAddresses {
   static baseAddress: string = 'https://moneymapapi.msoltanpour.ca/api/v1';
-  static createCategory: string = '/categories/add';
+  static category: string = '/categories';
+  static add: string = '/add';
+  static edit: string = '/edit';
+  static details: string = '/get';
+  static delete: string = '/delete';
+  static all: string = '/all';
 }
