99f4b5a0dd6386ca3b8aad8b8abd29c0e62e5002
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-19 14:42:16 -0300

    Complete validation component

--- src/app/components/shared/validation-messages/validation-messages.component.ts
+++ src/app/components/shared/validation-messages/validation-messages.component.ts
@@ -1,12 +1,27 @@
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-validation-messages',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './validation-messages.component.html',
 })
 export class ValidationMessagesComponent {
   @Input() control: AbstractControl | null = null;
   @Input() label: string = '';
+
+  unknownError(): boolean {
+    if (!this.control?.errors) return false;
+    const known = [
+      'required',
+      'minlength',
+      'maxlength',
+      'email',
+      'pattern',
+      'min',
+      'max',
+    ];
+    return Object.keys(this.control.errors).some((err) => !known.includes(err));
+  }
 }
