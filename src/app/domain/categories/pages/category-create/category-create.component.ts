ffc6effa3ee390b99543b4925dbfb3f87494e5a4
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-19 12:11:14 -0300

    Create  transaction form

--- src/app/domain/categories/pages/category-create/category-create.component.ts
+++ src/app/domain/categories/pages/category-create/category-create.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BreadcrumbComponent } from '../../../../components/common/breadcrumb/breadcrumb/breadcrumb.component';
 import { RouterModule } from '@angular/router';
+import { MainTitleComponent } from '../../../../components/common/main-title/main-title.component';
 
 @Component({
   selector: 'app-category-create',
-  imports: [BreadcrumbComponent, RouterModule],
+  imports: [BreadcrumbComponent, RouterModule, MainTitleComponent],
   templateUrl: './category-create.component.html',
 })
 export class CategoryCreateComponent {
