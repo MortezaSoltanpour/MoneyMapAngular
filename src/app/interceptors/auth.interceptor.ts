641af1b94ecf59c3c3c6f5de299c331dd3d8303a
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-22 20:16:51 -0300

    401 globally

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,17 +1,36 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  if (req.url.includes('/auth/login')) return next(req);
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  // Skip login or refresh requests
+  if (req.url.includes('/auth/login') || req.url.includes('/auth/refresh')) {
+    return next(req);
+  }
+
   const token = localStorage.getItem('access_token');
 
   if (token) {
-    const authReq = req.clone({
+    req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
       },
     });
-    return next(authReq);
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error) => {
+      if (error.status === 401) {
+        authService.logout();
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
