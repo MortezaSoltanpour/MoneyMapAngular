ca29d07865dda7b51be90fdbe0a550554f25a5b9
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-24 18:36:29 -0300

    Transaction routing

--- src/app/domain/transactions/transactions.routes.ts
+++ src/app/domain/transactions/transactions.routes.ts
@@ -1,10 +1,34 @@
 import { Routes } from '@angular/router';
 import { TransactionListComponent } from './pages/transaction-list/transaction-list.component';
+import { TransactionCreateComponent } from './pages/transaction-create/transaction-create.component';
+import { TransactionEditComponent } from './pages/transaction-edit/transaction-edit.component';
+import { TransactionDetailsComponent } from './pages/transaction-details/transaction-details.component';
+import { TransactionDeleteComponent } from './pages/transaction-delete/transaction-delete.component';
 
 export const transactionRoutes: Routes = [
   {
     path: '',
     component: TransactionListComponent,
     data: { title: 'Transactions' },
+  },
+  {
+    path: 'create',
+    component: TransactionCreateComponent,
+    data: { title: 'Create' },
+  },
+  {
+    path: 'edit',
+    component: TransactionEditComponent,
+    data: { title: 'Edit' },
+  },
+  {
+    path: 'details',
+    component: TransactionDetailsComponent,
+    data: { title: 'Details' },
+  },
+  {
+    path: 'delete',
+    component: TransactionDeleteComponent,
+    data: { title: 'Delete' },
   },
 ];
