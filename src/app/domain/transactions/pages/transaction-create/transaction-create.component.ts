4866413b2337839be2fc15abbf492378525392e1
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-24 18:48:41 -0300

    Design create form

--- src/app/domain/transactions/pages/transaction-create/transaction-create.component.ts
+++ src/app/domain/transactions/pages/transaction-create/transaction-create.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { MainTitleComponent } from '../../../../components/common/main-title/main-title.component';
+import { BreadcrumbComponent } from '../../../../components/common/breadcrumb/breadcrumb/breadcrumb.component';
+import { ValidationMessagesComponent } from '../../../../components/shared/validation-messages/validation-messages.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-transaction-create',
-  imports: [],
+  imports: [
+    MainTitleComponent,
+    BreadcrumbComponent,
+    ReactiveFormsModule,
+    ValidationMessagesComponent,
+  ],
   templateUrl: './transaction-create.component.html',
 })
-export class TransactionCreateComponent {}
+export class TransactionCreateComponent {
+  title = 'Create';
+
+  pageForm = new FormGroup({
+    Description: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(500),
+    ]),
+    Amount: new FormControl(null, [
+      Validators.required,
+      Validators.min(0.01),
+      Validators.pattern(/^\d+(\.\d{1,2})?$/),
+    ]),
+    IdCategory: new FormControl('', [Validators.required]),
+  });
+
+  handleSubmit() {}
+}
