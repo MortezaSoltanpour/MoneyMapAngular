0f37c5c82adffd60066a3c56b8f1626049b96e46
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-26 00:00:00 -0300

    Create transaction

--- src/app/domain/transactions/models/transactionDto.ts
+++ src/app/domain/transactions/models/transactionDto.ts
@@ -2,4 +2,8 @@ export interface transactionDto {
   description: string;
   amount: number;
   Idcategory: string;
+  dateRegistered?: Date | null;
+  category?: string | null;
+  fileAttached?: string | null;
+  idTransaction?: string | null;
 }
--- src/app/domain/transactions/pages/transaction-list/transaction-list.component.ts
+++ src/app/domain/transactions/pages/transaction-list/transaction-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TableComponent } from '../../../../components/shared/table/table.component';
 import {
   userSampleTableColumns,
@@ -6,14 +6,55 @@ import {
 } from '../../../../shared/models/sampledata';
 import { BreadcrumbComponent } from '../../../../components/common/breadcrumb/breadcrumb/breadcrumb.component';
 import { MainTitleComponent } from '../../../../components/common/main-title/main-title.component';
+import { TransactionServicesService } from '../../services/transaction-services.service';
+import { LoadingService } from '../../../../services/loading.service';
+import { finalize } from 'rxjs';
+import { transactionDto } from '../../models/transactionDto';
+import { resetFakeAsyncZone } from '@angular/core/testing';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [TableComponent, BreadcrumbComponent, MainTitleComponent],
+  imports: [
+    NgFor,
+    RouterModule,
+    CommonModule,
+    TableComponent,
+    BreadcrumbComponent,
+    MainTitleComponent,
+  ],
   templateUrl: './transaction-list.component.html',
 })
-export class TransactionListComponent {
+export class TransactionListComponent implements OnInit {
   userTableColumns = userSampleTableColumns;
   userTableData = userSampleTableData;
   title = 'Transactions';
+
+  constructor(
+    private services: TransactionServicesService,
+    private loading: LoadingService
+  ) {}
+
+  transactions: transactionDto[] = [];
+
+  ngOnInit(): void {
+    this.loading.show();
+
+    this.services
+      .get()
+      .pipe(
+        finalize(() => {
+          this.loading.hide();
+        })
+      )
+      .subscribe({
+        next: (response) => {
+          this.transactions = response.payLoad;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
 }
